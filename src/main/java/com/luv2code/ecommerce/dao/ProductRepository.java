2f9d761bd5e6986ad1c242aa63797c66b603973a
KhoalinhNguyen <khoalinhnguyen123@gmail.com>
2024-05-16 10:29:47 +0200

    Add findProductByName query

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -14,4 +14,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
     //query method, match by category id, use "id" as parameter value
     // --> expose endpoint ../search/findByCategoryId?id=1
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+    //Select * From Product p
+    //Where
+    //p.name LIKE CONCAT ('%', :name , '%')
 }
