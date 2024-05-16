b5691aefa5828ae626e1a5cd7a9a251ccd41c6a2
KhoalinhNguyen <khoalinhnguyen123@gmail.com>
2024-05-16 09:58:41 +0200

    Share to Github

---
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    //query method, match by category id, use "id" as parameter value
+    // --> expose endpoint ../search/findByCategoryId?id=1
+}
